a85f090050adb6500d5fc5de34e33984a35d5352
Paul King <PaulKing09@bigpond.com>
2021-12-10 20:15:27 +1100

    Added Main menu with start and quit buttons
    - Main Menu scene shows splash screen image as a background
    - Start button loads the main scene
    - Quit button quits the application and will stop the game if running inside the editor

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sceneBuildIndex"></param>
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+}
